ca729027999743ca60f045de48115fd264d59a43
ladn0 <84666429+kodika-dangerlyoha@users.noreply.github.com>
2025-06-06 21:31:39 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,13 @@ const toggle_burger_menu = () => {
     }
     document.querySelector('.burgerMenu').classList.add('burgerMenu_active');
 }
+
+const open_autopark = (title) => {
+    document.querySelector('.autopark__grid_active').classList.remove('autopark__grid_active');
+    document.querySelector('.equipment__grid_active').classList.remove('equipment__grid_active');
+    document.querySelector('._active').classList.remove('_active');
+
+    document.querySelector(`#grid_${title}`).classList.add('autopark__grid_active');
+    document.querySelector(`#eq_${title}`).classList.add('equipment__grid_active');
+    document.querySelector(`#button_${title}`).classList.add('_active');
+}
