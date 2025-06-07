a954638912e52bc80934c1d4363e20793925c626
ladn0 <84666429+kodika-dangerlyoha@users.noreply.github.com>
2025-06-07 23:02:15 +0300

    Add files via upload

---
+++ scripts/elements.js
@@ -0,0 +1,3 @@
+const get_big_pic_html = (link) => {
+    return `<img src="img/${link}" alt="">`;
+}
--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,20 @@ const toggle_burger_menu = () => {
     document.querySelector('.burgerMenu').classList.add('burgerMenu_active');
 }
 
+const toggle_big_pic = (img) => {
+    if (img) {
+        document.querySelector('.bigPicture').classList.add('bigPicture_active');
+        document.querySelector('.bigPicture').innerHTML = get_big_pic_html(img);
+        return
+    }
+    document.querySelector('.bigPicture').classList.remove('bigPicture_active');
+}
+
+const eq_for = {
+    'tLoader': "Телескопические погрузчики",
+    'mLoader': "Минипогрузчики",
+}
+
 const open_autopark = (title) => {
     document.querySelector('.autopark__grid_active').classList.remove('autopark__grid_active');
     document.querySelector('.equipment__grid_active').classList.remove('equipment__grid_active');
@@ -14,4 +28,6 @@ const open_autopark = (title) => {
     document.querySelector(`#grid_${title}`).classList.add('autopark__grid_active');
     document.querySelector(`#eq_${title}`).classList.add('equipment__grid_active');
     document.querySelector(`#button_${title}`).classList.add('_active');
+
+    document.querySelector('#equipment_for').textContent = eq_for[title];
 }
